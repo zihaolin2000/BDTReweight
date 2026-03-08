2a8c7488529841a7a7fec9b2c7489992db3dddbd
Lorenzo Giannessi <lorenzo.giannessi@unige.ch>
2026-03-08 12:06:49 +0100

    edits

--- target_selection.py
+++ target_selection.py
@@ -10,6 +10,10 @@ def perform_selection(input_file, output_file, sample, max_events=-1):
     f = NUISANCEFile(filepath=input_file, treename=treename, relevant_keys=None)
     num_entries = len(f)
 
+    print(f"Performing selection on {max_events} events from file {input_file}. \nSample: {sample}")
+
+    if max_events < 0:
+        max_events = num_entries
     selection_mask = np.zeros(num_entries, dtype=bool)
 
     pbar = tqdm(total=100)
@@ -18,7 +22,7 @@ def perform_selection(input_file, output_file, sample, max_events=-1):
         if i > max_events and max_events > 0:
             break
         # progress bar
-        if (i + 1) % (num_entries // 100) == 0:
+        if (i + 1) % (max_events // 100) == 0:
             pbar.update(1)
         # Perform selection
         if sample == "CCQELike":
@@ -29,16 +33,22 @@ def perform_selection(input_file, output_file, sample, max_events=-1):
         selection_mask[i] = selected
     pbar.close()
 
+    print(f"Selected {np.sum(selection_mask)} out of {max_events} events for sample {sample}.\nConverting branhces to awkward arrays and applying selection mask.")
     # Apply selection mask to all branches
     filtered_branches = {}
     tree = f.get_tree()
+    n_branches = len(f.keys)
     for branch_name in f.keys:
+        print(f"  Processing branch {branch_name} ({len(filtered_branches)+1}/{n_branches})...")
         branch_array = tree[branch_name].array(library="ak")
         filtered_branches[branch_name] = branch_array[selection_mask]
 
+    print(f"Filtered branches created. Writing to output file {output_file}...")
     # Write to new ROOT file
     with uproot.recreate(output_file) as fout:
-        fout.mktree(treename, filtered_branches)
+        # fout.mktree(treename, filtered_branches)
+        fout[treename] = filtered_branches
+
 
     print(f"Selection complete. Output written to {output_file}. Selected {np.sum(selection_mask)} out of {num_entries} events.")
 
@@ -48,7 +58,8 @@ if __name__ == "__main__":
     parser.add_argument('--input_file', '-i', type=str, required=True, help='Path to the input NUISANCE flat tree file.')
     parser.add_argument('--output_file', '-o', type=str, required=True, help='Path to the output ROOT file.')
     parser.add_argument('--sample', '-s', type=str, default='CCQELike', help='Sample type (e.g., "CCQELike").')
+    parser.add_argument('--max_events', '-m', type=int, default=-1, help='Maximum number of events to process (default: -1 for all events).')
 
     args = parser.parse_args()
 
-    perform_selection(args.input_file, args.output_file, args.sample)
+    perform_selection(args.input_file, args.output_file, args.sample, args.max_events)
